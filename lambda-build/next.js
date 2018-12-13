0e31b134bc87bf75a0246bf0f0b9a8ae127e6e66
Bryan Robinson <bryanlrobinson@gmail.com>
2018-12-13 15:30:18 -0600

    Adds conditions for first and last in previous and next functions

--- lambda-build/next.js
+++ lambda-build/next.js
@@ -9,15 +9,22 @@ const findSite = (url) => {
     return site;
 }
 
+const isLast = (siteId) => {
+    return siteId === sites.length - 1 ? true : false
+}
+
 exports.handler = function(event, context, callback) {
     const uri = event.headers.host;
     const siteId = findSite(uri);
-    const nextSite = sites[siteId + 1];
+
+    const lastCheck = isLast(siteId);
+    console.log(lastCheck);
+    const nextSite = lastCheck ? `${sites[0].uri}` : `${sites[siteId + 1].uri}`;
     console.log(nextSite);
     var response = {
         statusCode: 302,
         headers: {
-            "Location": nextSite.uri
+            "Location": nextSite
         },
         body: `event is ${event}`
     };

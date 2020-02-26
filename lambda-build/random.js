e627bc86ea4cc25a0214ef81ca0eaf78e113ad4b
Jonas Brusman <jonas@brusman.se>
2020-02-26 12:48:47 +0100

    Don't cache random site
    Fixes #2

--- lambda-build/random.js
+++ lambda-build/random.js
@@ -1,10 +1,10 @@
 'use strict';
 const sites = require('../data/sites.json');
 
-const randomSite = sites[Math.floor(Math.random()*sites.length)];
-console.log(randomSite);
-
 exports.handler = function(event, context, callback) {
+    const randomSite = sites[Math.floor(Math.random()*sites.length)];
+    console.log(randomSite);
+
     var response = {
         statusCode: 302,
         headers: {
